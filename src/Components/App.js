b249ae2c7cee7dc47b5722adce12f38c63e95cc7
stella <sftamesis13@gmail.com>
2022-10-26 12:28:59 -0700

    add more components and fix error for App.css

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes as Switch} from 'react-router-dom'
 import Home from './Home'
+import Header from './Header';
 import NewHikeLog from './NewHikeLog';
+import NavBar from './NavBar';
 import './App.css'
 
 function App() {
   return (
-   <div className="App">
+    <div className="App">
       <Router>
         <NavBar />
         <Header />
@@ -18,7 +20,7 @@ function App() {
           <Route exact path="/">
             <Home />
           </Route>
-          
+
         </Switch>
       </Router>
     </div>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,7 +6,6 @@ function Home() {
             <h1>VentureLog</h1>
             <h2>A Wonderers Footprint</h2>
             <hr/>
-            <p>This Application gives hiker enthusiasts to document their Adventures</p>
         </div>
     )
 }
